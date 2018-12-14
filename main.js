3b02c80dba6f9f4ebd3f21c779c4032c3e16ba20
romanhavr <romanhavr@ukr.net>
2018-12-14 13:27:44 +0200

    with javascript

---
+++ main.js
@@ -0,0 +1,72 @@
+works = [
+  {
+    demoUrl: 'projects/screensaver/screensaver.html',
+    codeUrl: 'https://github.com/romanhavr/screensaver',
+    description: 'Simple screensaver coded with JavaScript.',
+    imageUrl: 'images/screensaver.jpg',
+    imageAlt: 'screensaver'
+  },
+  {
+    demoUrl: 'https://romanhavr.github.io',
+    codeUrl: 'https://github.com/romanhavr/romanhavr.github.io',
+    description: 'This Portfolio.',
+    imageUrl: 'images/portfolio.jpg',
+    imageAlt: 'portfolio'
+  },
+  {
+    demoUrl: 'https://todo-app-my.herokuapp.com/',
+    codeUrl: 'https://github.com/romanhavr/todo-app',
+    description: 'Todo app created with ReactJS.',
+    imageUrl: 'images/todo-app.jpg',
+    imageAlt: 'todo-app'
+  },
+  {
+    demoUrl: 'http://my-internet-shop.herokuapp.com',
+    codeUrl: 'https://github.com/romanhavr/Apiko-Fall-Intensive-2018-Final-Test',
+    description: 'SPA created with ReactJS (Redux, Routing...).',
+    imageUrl: 'images/my-internet-shop.jpg',
+    imageAlt: 'my-internet-shop'
+  },
+];
+
+works.map( work => {  
+
+  let table = document.querySelector('.table');
+  let rowDescription = document.createElement('div');
+  let rowImage = document.createElement('div');
+  let image = document.createElement('img');
+  let rowButtons = document.createElement('div');
+  let spanDemo = document.createElement('span');
+  let spanCode = document.createElement('span');
+  let aDemo = document.createElement('a');
+  let aCode = document.createElement('a');
+  let cell = document.createElement('div');
+
+  rowDescription.className = 'row';
+  rowImage.className = 'row';
+  rowButtons.className= 'row';
+  spanDemo.className= 'button';
+  spanCode.className= 'button';
+  cell.className = 'cell';
+
+  rowDescription.innerHTML = work.description;
+  cell.appendChild(rowDescription);
+
+  image.src = work.imageUrl;
+  image.alt = work.imageAlt;
+  rowImage.appendChild(image);
+  cell.appendChild(rowImage);
+
+  aDemo.innerHTML = 'DEMO';
+  aDemo.href = work.demoUrl;
+  aCode.innerHTML = 'CODE';
+  aCode.href = work.codeUrl;
+  spanDemo.appendChild(aDemo);
+  spanCode.appendChild(aCode);
+  rowButtons.appendChild(spanDemo);
+  rowButtons.appendChild(spanCode);
+  cell.appendChild(rowButtons);
+
+  table.appendChild(cell);
+  }
+)
